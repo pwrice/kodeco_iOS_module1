44bd13d2e860b1bae7980aefa9c3b7cca1e1ba07
Pete Rice <peteza011@gmail.com>
2024-12-27 11:43:38 -0800

    minor reorganization of network code

--- module13and14/LoopCanvas/LoopCanvas/Models/SampleSetStore.swift
+++ module13and14/LoopCanvas/LoopCanvas/Models/SampleSetStore.swift
@@ -36,9 +36,9 @@ struct RemoteSampleSetIndex: Codable {
 
 enum RemoteSampleSetIndexLoadingState {
   case notLoaded,
-       loading,
-       loaded,
-       error
+   loading,
+   loaded,
+   error
 }
 
 // Index File URL
@@ -202,45 +202,4 @@ class SampleSetStore: ObservableObject {
 
 // Session Loading Utilities
 
-protocol URLSessionLoading {
-  func fetchDataFromURL(urlRequest: URLRequest, completionHandler: @escaping (Data?, URLResponse?, Error?) -> Void)
-}
 
-
-class URLSessionLoader: URLSessionLoading {
-  func fetchDataFromURL(urlRequest: URLRequest, completionHandler: @escaping (Data?, URLResponse?, Error?) -> Void) {
-    URLSession.shared
-      .dataTask(with: urlRequest, completionHandler: completionHandler)
-      .resume()
-  }
-}
-
-class MockURLSessionLoader: URLSessionLoading {
-  let mockData: Data?
-  let mockResponse: URLResponse
-  let mockError: Error?
-  var completionHandler: ((Data?, URLResponse?, Error?) -> Void)?
-  var lastFetchURLRequest: URLRequest?
-
-  convenience init(mockDataUrl: URL, mockResponse: URLResponse, mockError: Error?) {
-    let unstructuredData = try! Data(contentsOf: mockDataUrl)
-    self.init(mockData: unstructuredData, mockResponse: mockResponse, mockError: mockError)
-  }
-
-  init(mockData: Data?, mockResponse: URLResponse, mockError: Error?) {
-    self.mockData = mockData
-    self.mockResponse = mockResponse
-    self.mockError = mockError
-  }
-
-  func fetchDataFromURL(urlRequest: URLRequest, completionHandler: @escaping (Data?, URLResponse?, Error?) -> Void) {
-    self.lastFetchURLRequest = urlRequest
-    self.completionHandler = completionHandler
-  }
-
-  func resolveCompletionHandler() {
-    if let completionHandler = completionHandler {
-      completionHandler(mockData, mockResponse, mockError)
-    }
-  }
-}
